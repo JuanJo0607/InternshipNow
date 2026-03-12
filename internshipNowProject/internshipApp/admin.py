dff76d818ce71e6ae771418a3deb2eed27d4ebb1
TomasOspina0108 <123612236+TomasOspina0108@users.noreply.github.com>
2026-03-12 11:27:49 -0500

    Revert "Feature/us 06 offers"

--- internshipNowProject/internshipApp/admin.py
+++ internshipNowProject/internshipApp/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-from .models import User, StudentProfile, CompanyProfile, InternshipOffer
-
-
-@admin.register(InternshipOffer)
-class InternshipOfferAdmin(admin.ModelAdmin):
-    list_display = ('title', 'company', 'status', 'created_at')
-    list_filter = ('status', 'company')
-    search_fields = ('title', 'description', 'requirements', 'desired_skills')
+from .models import User, StudentProfile, CompanyProfile
 
 admin.site.register(User)
 admin.site.register(StudentProfile)
