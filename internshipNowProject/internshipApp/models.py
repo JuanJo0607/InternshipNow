c5d8fffb459d73da049b4d2d279ed7e85dff0cd9
Tomas Ospina <tospina@idtsas.com>
2026-04-17 16:39:42 -0500

    Front en inlges, seleccionar carrera y quemar universidad

--- internshipNowProject/internshipApp/models.py
+++ internshipNowProject/internshipApp/models.py
@@ -8,19 +8,20 @@ class User(AbstractUser):
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
+
 class StudentProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    university = models.CharField(max_length=255)
+    university = models.CharField(max_length=255, default='Universidad EAFIT')
     career = models.CharField(max_length=255)
-    skills = models.TextField(blank=True)  
+    skills = models.TextField(blank=True)
     bio = models.TextField(blank=True)
-    is_verified = models.BooleanField(default=False)  # <-- línea nueva
+    is_verified = models.BooleanField(default=False)
+    cv_pdf = models.FileField(upload_to='cvs/', blank=True, null=True)
 
     def __str__(self):
         if self.is_verified:
             return f"{self.user.username} - Estudiante Verificado"
         return f"{self.user.username} - Pendiente de verificación"
-    cv_pdf = models.FileField(upload_to='cvs/', blank=True, null=True)  # US-10: upload CV
 
 
 class CompanyProfile(models.Model):
